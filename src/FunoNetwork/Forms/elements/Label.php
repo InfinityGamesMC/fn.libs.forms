c76baffceb5e0d02e8f5274ff8686f1e27a2068d
Luke <luke.wyatt21@gmail.com>
2019-04-23 18:39:00 +0100

    Fix crash with labels

--- src/FunoNetwork/Forms/elements/Label.php
+++ src/FunoNetwork/Forms/elements/Label.php
@@ -7,6 +7,10 @@ class Label extends Element {
     public function __construct(string $text) {
         parent::__construct($text);
     }
+	
+	public function getCallback() {
+		return null;
+	}
 
     protected function serialize(): array {
         return [
