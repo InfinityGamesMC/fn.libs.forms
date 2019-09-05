93b70cf57149f3b8d796c5193628281ab6ddb16e
Luke <luke.wyatt21@gmail.com>
2019-09-05 14:30:21 +0100

    Implement image support for buttons

--- src/FunoNetwork/Forms/elements/Button.php
+++ src/FunoNetwork/Forms/elements/Button.php
@@ -4,10 +4,19 @@ namespace FunoNetwork\Forms\elements;
 
 class Button extends Element {
     private $callback;
+    private $image;
 
     public function __construct(string $text, ?callable $callback) {
         parent::__construct($text);
         $this->callback = $callback;
+    }
+
+    public function setImage(Image $image): void {
+        $this->image = $image;
+    }
+
+    public function getImage(): ?Image {
+        return $this->image;
     }
 
     public function getCallback(): ?callable {
@@ -15,6 +24,11 @@ class Button extends Element {
     }
 
     protected function serialize(): array {
+        if($this->image !== null) {
+            return [
+                "image" => $this->image
+            ];
+        }
         return [];
     }
 }
